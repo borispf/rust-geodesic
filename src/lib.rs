252b955579fb89f40a072754e7f560d5fa1f99f7
Boris Pfahringer <bpnzltd@gmail.com>
2016-02-20 15:17:06 +0100

    rust-fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-#[allow(non_snake_case, dead_code, non_camel_case_types)] mod geodesic;
+#[allow(non_snake_case, dead_code, non_camel_case_types)]
+mod geodesic;
 
 pub struct Geodesic {
     g: geodesic::Struct_geod_geodesic,
@@ -7,21 +8,41 @@ pub struct Geodesic {
 impl Geodesic {
     pub fn new(a: f64, f: f64) -> Geodesic {
         let mut g = geodesic::Struct_geod_geodesic::default();
-        unsafe { geodesic::geod_init(&mut g, a, f); }
+        unsafe {
+            geodesic::geod_init(&mut g, a, f);
+        }
         Geodesic { g: g }
     }
     pub fn direct(&self, lat1: f64, lon1: f64, azi1: f64, s12: f64) -> (f64, f64, f64) {
         let mut lat2 = std::f64::NAN;
         let mut lon2 = std::f64::NAN;
         let mut azi2 = std::f64::NAN;
-        unsafe { geodesic::geod_direct(&self.g, lat1, lon1, azi1, s12, &mut lat2, &mut lon2, &mut azi2); }
+        unsafe {
+            geodesic::geod_direct(&self.g,
+                                  lat1,
+                                  lon1,
+                                  azi1,
+                                  s12,
+                                  &mut lat2,
+                                  &mut lon2,
+                                  &mut azi2);
+        }
         (lat2, lon2, azi2)
     }
     pub fn inverse(&self, lat1: f64, lon1: f64, lat2: f64, lon2: f64) -> (f64, f64, f64) {
         let mut s12 = std::f64::NAN;
         let mut azi1 = std::f64::NAN;
         let mut azi2 = std::f64::NAN;
-        unsafe { geodesic::geod_inverse(&self.g, lat1, lon1, lat2, lon2, &mut s12, &mut azi1, &mut azi2); }
+        unsafe {
+            geodesic::geod_inverse(&self.g,
+                                   lat1,
+                                   lon1,
+                                   lat2,
+                                   lon2,
+                                   &mut s12,
+                                   &mut azi1,
+                                   &mut azi2);
+        }
         (s12, azi1, azi2)
     }
 }
